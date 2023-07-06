dc04600b02a8be1b3daa07a8e86f189566d7bbdf
Djonatan Bonelli <100157600+FenrirTG1@users.noreply.github.com>
2023-07-06 08:26:34 -0300

    Update elevador.vhdl

--- elevador.vhdl
+++ elevador.vhdl
@@ -5,83 +5,106 @@ use ieee.numeric_std.all;
 --3 clocks para iniciar
 --cada bit é um andar (0001 = primeiro andar; 1000 = quarto andar)...
 
-ENTITY elevador IS
-    PORT (Clock, Reset : IN STD_LOGIC;
-    Escolha_andar : IN STD_LOGIC_VECTOR(3 downto 0);
-    Subir, Descer, parar : OUT STD_LOGIC; 
-    Sensor_andar: OUT STD_LOGIC_VECTOR(3 downto 0));
-END elevador;
+ENTITY teste IS
+    PORT (KEY : IN STD_LOGIC_VECTOR(3 downto 0);	--Clock, reset
+    SW : IN STD_LOGIC_VECTOR(9 downto 0); --Escolha_andar
+    LEDR: OUT STD_LOGIC_VECTOR(9 downto 0); --Subir, Descer, Parar
+    LEDG: OUT STD_LOGIC_VECTOR(7 downto 0)); --Sensor_andar
+END teste;
 
-ARCHITECTURE Behavior OF elevador IS
+ARCHITECTURE Behavior OF teste IS
     TYPE estado_elevador IS (subindo, descendo, parado);
-    SIGNAL estado_atual, proximo_estado : estado_elevador;
+    SIGNAL estado_atual: estado_elevador;
+	 SIGNAL s_sensor_andar: std_logic_vector(3 downto 0);
+	 
 BEGIN
-    PROCESS (Reset, Clock)
-	variable s_sensor_andar: std_logic_vector(3 downto 0);
+
+	PROCESS (sw(9), KEY(3))
+	variable int_sensor : integer;
+	variable int_escolha: integer; 
+	
 begin
-        if Reset = '1' then
-            s_sensor_andar := "0000";      --terreo
-	    estado_atual <= parado;
+        if sw(9) = '1' then   --reset
+            s_sensor_andar <= "0000";      --terreo
+				estado_atual <= parado;
+				LEDG(0) <= s_sensor_andar(0);
+				LEDG(1) <= s_sensor_andar(1);
+				LEDG(2) <= s_sensor_andar(2);
+				LEDG(3) <= s_sensor_andar(3);
+				
+        elsif key(3)'event and KEY(3) = '1' then --clock
+			 int_sensor := to_integer((unsigned(s_sensor_andar)));
+			 int_escolha := to_integer((unsigned(SW)));
+
+			 case Estado_atual is
+			
+				  when Parado =>
+						if int_sensor = int_escolha then --se estiver no andar certo: parado
+							 estado_atual <= parado;
+						else
+							 if int_sensor > 8 then      --passou o limite
+								  estado_atual <= parado;
+							 elsif int_sensor < int_escolha then    --se estiver no andar inferior a escolha: subindo
+								  estado_atual <= subindo;
+							 elsif int_sensor > int_escolha then       --se estiver no andar superior a escolha: descendo
+								  estado_atual <= descendo;
+							 END IF;
+						END IF;
+
+				  when Subindo =>
+						if int_sensor = int_escolha then --se estiver no andar certo: parado
+							 estado_atual <= parado;
+						else
+							if s_sensor_andar = "0000" then
+								 s_sensor_andar <= "0001";
+							elsif int_sensor >= 8 then
+								 estado_atual <= parado;
+							else
+								s_sensor_andar <= std_logic_vector(shift_left(unsigned(s_sensor_andar), 1));-- desloca bits a esquerda
+							END IF;
+						end if;
+
+				  when Descendo =>
+						if int_sensor = int_escolha then --se estiver no andar certo: parado
+							 estado_atual <= parado;
+						else		  
+							 if int_sensor > 8 then
+								s_sensor_andar <= "1000";
+							 elsif s_sensor_andar = "0000" then
+								estado_atual <= parado;
+							 else
+								s_sensor_andar <= std_logic_vector(shift_right(unsigned(s_sensor_andar), 1));-- desloca bits a direita
+							 END IF;
+						end if;
+						
+				  END CASE;
+				  -- atribui saidas
+				  
+						LEDG(0) <= s_sensor_andar(0);
+						LEDG(1) <= s_sensor_andar(1);
+						LEDG(2) <= s_sensor_andar(2);
+						LEDG(3) <= s_sensor_andar(3);
+						
+						if estado_atual = subindo then
+							LEDR(2) <= '1';
+						else
+							LEDR(2) <= '0';
+						end if;
+						
+						if	estado_atual = descendo then
+							LEDR(0) <= '1';
+						else
+							LEDR(0) <= '0';
+						end if;
+						
+						if estado_atual = parado then
+							LEDR(1) <= '1';
+						else
+							LEDR(1) <= '0';
+						end if;
+
         END IF;
-	
-	Estado_atual <= parado;
-
-        if rising_edge(clock) then
-	        estado_atual <= proximo_estado;
-	
-        END IF;
-    END PROCESS;
-
-    PROCESS (Estado_atual, Proximo_estado, escolha_andar)
-        variable int_sensor : integer;
-        variable int_escolha: integer;   
-        variable s_sensor_andar : std_logic_vector(3 downto 0);
-begin
-    int_sensor := to_integer((unsigned(s_sensor_andar)));
-    int_escolha := to_integer((unsigned(escolha_andar)));
-
-    case Estado_atual is
-	
-        when Parado =>   
-            if int_sensor = int_escolha then --se estiver no andar certo: parado
-                Proximo_estado <= parado;
-            else
-                if int_sensor > 8 then      --passou o limite
-                    Proximo_estado <= parado;
-                elsif int_sensor < int_escolha then    --se estiver no andar inferior a escolha: subindo
-                    Proximo_estado <= subindo;
-                elsif int_sensor > int_escolha then       --se estiver no andar superior a escolha: descendo
-                    Proximo_estado <= descendo;  
-		else
-		    Proximo_estado <= parado; 
-                END IF;
-            END IF;
-
-        when Subindo =>
-
-        --tratamentos gerais
-            if s_sensor_andar = "0000" then
-                s_sensor_andar := "0001";
-            elsif int_escolha > 8 then
-                proximo_estado <= parado;
-   	    else
-		s_sensor_andar := std_logic_vector(shift_left(unsigned(s_sensor_andar), 1));-- desloca bits a esquerda
-            END IF;
-
-        when Descendo =>
-	
-	    if to_integer(unsigned(s_sensor_andar)) > 8 then
-		s_sensor_andar := "1000";
-	    else
-		s_sensor_andar := std_logic_vector(shift_right(unsigned(s_sensor_andar), 1));-- desloca bits a direita	           
-		END IF;
-
-        END CASE;
-        -- atribui saidas
-	    sensor_andar <= s_sensor_andar; 
-            Subir <= '1' when estado_atual = subindo else '0';
-            Descer <=  '1' when estado_atual = descendo else '0';
-	    Parar <=  '1' when estado_atual = parado else '0';
+		  
 	END PROCESS;
         
 END Behavior;
