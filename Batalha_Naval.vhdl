b1afc13810824a3ebeb1aab1a26a1df671f260b4
Djonatan Bonelli <100157600+FenrirTG1@users.noreply.github.com>
2023-07-12 09:25:38 -0300

    Update Batalha_Naval.vhdl

--- Batalha_Naval.vhdl
+++ Batalha_Naval.vhdl
@@ -4,7 +4,7 @@ use ieee.numeric_std.all;
 use ieee.std_logic_unsigned.all;
 
 -- Entidade principal
-ENTITY BN IS
+ENTITY BatalhaNaval IS
     PORT(
 
 ---------------------------------------------------------
@@ -41,88 +41,94 @@ ENTITY BN IS
 
 -------------------------------------------------------
 
-        -- Zero: "11111100"
-        -- Um: "01100000"
-        -- Dois: "11011010"
-        -- Três: "11110010"
-        -- Quatro: "01100110"
-        -- Cinco: "10110110"
-        -- Seis: "10111110"
+        -- Zero: "1111110"
+        -- Um: "0110000"
+        -- Dois: "1101101"
+        -- Três: "1111001"
+        -- Quatro: "0110011"
+        -- Cinco: "1011011"
+        -- Seis: "1011111"
 
-        -- DISPLAY ESQUERDA
-        jogadas_restantes: OUT STD_LOGIC_VECTOR(7 downto 0);    --Exibe a quantidade de jogadas restantes
+        -- hex0 ESQUERDA
+        hex1: OUT STD_LOGIC_VECTOR(6 downto 0);    --Exibe a quantidade de jogadas restantes
 
-        -- DISPLAY DIREITA
-        acertos_restantes: OUT STD_LOGIC_VECTOR(7 downto 0)    --Exibe a quantidade de acertos necessários
+        -- hex0 DIREITA
+        hex0: OUT STD_LOGIC_VECTOR(6 downto 0)    --Exibe a quantidade de acertos necessários
 
 -------------------------------------------------------
 
     );
-END BN;
+END BatalhaNaval;
 
 
 -- Cria tabuleiro
-ARCHITECTURE Behavior_Tabuleiro OF BN IS 
+ARCHITECTURE Behavior_Tabuleiro OF BatalhaNaval IS 
 
 --                                  indices da matriz (3 em inteiro)               Conteudo do indice (vetor de 4 bits)
-    TYPE matriz_tabuleiro IS ARRAY (natural range 0 to 3, natural range 0 to 3) of std_logic_vector(3 downto 0);
-    SIGNAL matriz : matriz_tabuleiro;   
+   TYPE matriz_tabuleiro IS ARRAY (natural range 0 to 3, natural range 0 to 3) of std_logic_vector(3 downto 0);
+   SIGNAL matriz : matriz_tabuleiro;
+	SIGNAL input_usuario: STD_LOGIC_VECTOR(3 downto 0);
 
 BEGIN
 
-    --Criação da matriz do tabuleiro
-    matriz(0, 0) <= "0110";
-    matriz(0, 1) <= "1101";
-    matriz(0, 2) <= "1000";
-    matriz(0, 3) <= "0011";
-    matriz(1, 0) <= "0000";
-    matriz(1, 1) <= "1001";
-    matriz(1, 2) <= "0001";
-    matriz(1, 3) <= "1011";
-    matriz(2, 0) <= "0111";
-    matriz(2, 1) <= "0100";
-    matriz(2, 2) <= "1110";
-    matriz(2, 3) <= "0010";
-    matriz(3, 0) <= "0101";
-    matriz(3, 1) <= "1111";
-    matriz(3, 2) <= "1010";
-    matriz(3, 3) <= "1100";
+	 --Criação da matriz do tabuleiro
+	 matriz(0, 0) <= "0110";
+	 matriz(0, 1) <= "1101";
+	 matriz(0, 2) <= "1000";
+	 matriz(0, 3) <= "0011";
+	 matriz(1, 0) <= "0000";
+	 matriz(1, 1) <= "1001";
+	 matriz(1, 2) <= "0001";
+	 matriz(1, 3) <= "1011";
+	 matriz(2, 0) <= "0111";
+	 matriz(2, 1) <= "0100";
+	 matriz(2, 2) <= "1110";
+	 matriz(2, 3) <= "0010";
+	 matriz(3, 0) <= "0101";
+	 matriz(3, 1) <= "1111";
+	 matriz(3, 2) <= "1010";
+	 matriz(3, 3) <= "1100";
+ 
+	--Liga as entradas ao sinal 
+	input_usuario(0) <= sw(3);
+	input_usuario(1) <= sw(4);
+	input_usuario(2) <= sw(5);
+	input_usuario(3) <= sw(6);
 
-    PROCESS (key(3), key(0))    -- Clock e Reset
+   PROCESS (key(3), key(0))    -- Clock e Reset
 
-    --Para salvar a codificacao das posicoes escolhidas
-	variable pos1 : STD_LOGIC_VECTOR(3 downto 0);
-	variable pos2: STD_LOGIC_VECTOR(3 downto 0);
-	variable pos3 : STD_LOGIC_VECTOR(3 downto 0);
-	variable pos4: STD_LOGIC_VECTOR(3 downto 0);
+   --Para salvar a codificacao das posicoes escolhidas
+	variable pos1 : STD_LOGIC_VECTOR(3 downto 0) := "0000";
+	variable pos2: STD_LOGIC_VECTOR(3 downto 0) := "0000";
+	variable pos3 : STD_LOGIC_VECTOR(3 downto 0) := "0000";
+	variable pos4: STD_LOGIC_VECTOR(3 downto 0) := "0000";
 
-    --Para salvar quais posicoes ja foram acertadas
-    variable acertos: STD_LOGIC_VECTOR(3 downto 0) := "0000";
+   --Para salvar quais posicoes ja foram acertadas
+   variable acertos: STD_LOGIC_VECTOR(3 downto 0) := "0000";
 
-    --Serve para separar o dado de entrada
-    variable linhaVet: std_logic_vector(1 downto 0);
-    variable colunaVet: std_logic_vector(1 downto 0);
+   --Serve para separar o dado de entrada
+   variable linhaVet: std_logic_vector(1 downto 0);
+   variable colunaVet: std_logic_vector(1 downto 0);
 
-    --Armazena a linha e coluna disparada
-    variable linha : integer;
-    variable coluna : integer;
+   --Armazena a linha e coluna disparada
+   variable linha : integer;
+   variable coluna : integer;
 
-    --Armazena a quantidade de jogadas restantes
-    variable jogadas : integer := 6;
+   --Armazena a quantidade de jogadas restantes
+   variable jogadas : integer := 6;
 
-    --Armazena a quantidade de acertos necessários
-    variable acertos_necessarios : integer := 4;
+   --Armazena a quantidade de acertos necessários
+   variable acertos_necessarios : integer := 4;
 
     BEGIN
 
     -- Reinicia o jogo, caso o reset seja pressionado ou as jogadas acabem ou acerte todos os pontos
-    IF (key(0) = '1') or (jogadas = 0) or (acertos_necessarios = 0) then
-        ledr(9) <= '0';
-        ledg(7) <= '0';     -- Acertou 
-        ledg(6) <= '0';     -- Errou     
-        jogadas_restantes <= "000";     -- Display 1
-        acertos_restantes <= "000";     -- Display 2
-        acertos := "0000";      
+    IF (key(0) = '0') or (jogadas = 0) or (acertos_necessarios = 0) then
+        ledr <= "0000000001";
+        ledg <= "00000000";     -- Acertou  
+        hex1 <= "0000000";     -- hex1
+        hex0 <= "1111111";     -- hex2
+        acertos := "0000";
         pos1 := "0000";
         pos2 := "0000";
         pos3 := "0000";
@@ -131,30 +137,31 @@ BEGIN
         acertos_necessarios := 4;
 
     -- Executa com o clock
-    ELSIF (key(3)'EVENT AND key(3) = '1') then
+    ELSIF (key(3)'EVENT AND key(3) = '0') then
 
         -- Reset das saídas
         ledr(9) <= '0';
         ledg(7) <= '0';
         ledg(6) <= '0';
 
+		
         -- Codificacao para o modo 0, inserir barcos
         IF sw(0) = '0' then
 
             -- Impede que sejam inseridos barcos em posições ja ocupadas
-            IF (pos1 = (sw(6) & sw(5) & sw(4) & sw(3))) or (pos2 = (sw(6) & sw(5) & sw(4) & sw(3))) or (pos3 = (sw(6) & sw(5) & sw(4) & sw(3))) or (pos4 = (sw(6) & sw(5) & sw(4) & sw(3))) then
+            IF (pos1 = input_usuario) or (pos2 = input_usuario) or (pos3 = input_usuario) or (pos4 = input_usuario) then
                 ledr(9) <= '1';   -- Input Inválido
 
             -- Codificacao para o barco 1
             ELSIF (sw(9) = '1') and (sw(8) = '0') and (sw(7) = '0') then
                 --Atribui diretamente à variavel
-                pos1 := (sw(6) & sw(5) & sw(4) & sw(3));
+                pos1 := input_usuario;
 
             -- Codificacao para o barco 2
             ELSIF (sw(9) = '0') and (sw(8) = '1') and (sw(7) = '0') then
 
                 --Atribui diretamente à variavel
-                pos2 := (sw(6) & sw(5) & sw(4) & sw(3));
+                pos2 := input_usuario;
 
             -- Codificacao para o barco 3
             ELSIF (sw(9) = '0') and (sw(8) = '0') and (sw(7) = '1') then
@@ -166,32 +173,32 @@ BEGIN
                         ledr(9) <= '1';
                     else
                         --Atribui diretamente a variavel
-                        pos3 := (sw(6) & sw(5) & sw(4) & sw(3));
+                        pos3 := input_usuario;
 
                         --Encontra a pos4
-                        IF (sw(6) & sw(5) & sw(4) & sw(3)) = matriz(0,0) then
+                        IF input_usuario = matriz(0,0) then
                             pos4 := matriz(0,1);
-                        ELSIF (sw(6) & sw(5) & sw(4) & sw(3)) = matriz(0,1) then
+                        ELSIF input_usuario = matriz(0,1) then
                             pos4 := matriz(0,2);
-                        ELSIF (sw(6) & sw(5) & sw(4) & sw(3)) = matriz(0,2) then
+                        ELSIF input_usuario = matriz(0,2) then
                             pos4 := matriz(0,3);
-                        ELSIF (sw(6) & sw(5) & sw(4) & sw(3)) = matriz(1,0) then
+                        ELSIF input_usuario = matriz(1,0) then
                             pos4 := matriz(1,1);
-                        ELSIF (sw(6) & sw(5) & sw(4) & sw(3)) = matriz(1,1) then
+                        ELSIF input_usuario = matriz(1,1) then
                             pos4 := matriz(1,2);
-                        ELSIF (sw(6) & sw(5) & sw(4) & sw(3)) = matriz(1,2) then
+                        ELSIF input_usuario = matriz(1,2) then
                             pos4 := matriz(1,3);
-                        ELSIF (sw(6) & sw(5) & sw(4) & sw(3)) = matriz(2,0) then
+                        ELSIF input_usuario = matriz(2,0) then
                             pos4 := matriz(2,1);
-                        ELSIF (sw(6) & sw(5) & sw(4) & sw(3)) = matriz(2,1) then
+                        ELSIF input_usuario = matriz(2,1) then
                             pos4 := matriz(2,2);
-                        ELSIF (sw(6) & sw(5) & sw(4) & sw(3)) = matriz(2,2) then
+                        ELSIF input_usuario = matriz(2,2) then
                             pos4 := matriz(2,3);
-                        ELSIF (sw(6) & sw(5) & sw(4) & sw(3)) = matriz(3,0) then
+                        ELSIF input_usuario = matriz(3,0) then
                             pos4 := matriz(3,1);
-                        ELSIF (sw(6) & sw(5) & sw(4) & sw(3)) = matriz(3,1) then
+                        ELSIF input_usuario = matriz(3,1) then
                             pos4 := matriz(3,2);
-                        ELSIF (sw(6) & sw(5) & sw(4) & sw(3)) = matriz(3,2) then
+                        ELSIF input_usuario = matriz(3,2) then
                             pos4 := matriz(3,3);
                         END IF;
 
@@ -203,32 +210,32 @@ BEGIN
                         ledr(9) <= '1';
                     else
                         --Atribui diretamente a variavel
-                        pos3 := (sw(6) & sw(5) & sw(4) & sw(3));
+                        pos3 := input_usuario;
 
                         --Encontra a pos4
-                        IF (sw(6) & sw(5) & sw(4) & sw(3)) = matriz(0,0) then
+                        IF input_usuario = matriz(0,0) then
                             pos4 := matriz(1,0);
-                        ELSIF (sw(6) & sw(5) & sw(4) & sw(3)) = matriz(1,0) then
+                        ELSIF input_usuario = matriz(1,0) then
                             pos4 := matriz(2,0);
-                        ELSIF (sw(6) & sw(5) & sw(4) & sw(3)) = matriz(2,0) then
+                        ELSIF input_usuario = matriz(2,0) then
                             pos4 := matriz(3,0);
-                        ELSIF (sw(6) & sw(5) & sw(4) & sw(3)) = matriz(0,1) then
+                        ELSIF input_usuario = matriz(0,1) then
                             pos4 := matriz(1,1);
-                        ELSIF (sw(6) & sw(5) & sw(4) & sw(3)) = matriz(1,1) then
+                        ELSIF input_usuario = matriz(1,1) then
                             pos4 := matriz(2,1);
-                        ELSIF (sw(6) & sw(5) & sw(4) & sw(3)) = matriz(2,1) then
+                        ELSIF input_usuario = matriz(2,1) then
                             pos4 := matriz(3,1);
-                        ELSIF (sw(6) & sw(5) & sw(4) & sw(3)) = matriz(0,2) then
+                        ELSIF input_usuario = matriz(0,2) then
                             pos4 := matriz(1,2);
-                        ELSIF (sw(6) & sw(5) & sw(4) & sw(3)) = matriz(1,2) then
+                        ELSIF input_usuario = matriz(1,2) then
                             pos4 := matriz(2,2);
-                        ELSIF (sw(6) & sw(5) & sw(4) & sw(3)) = matriz(2,2) then
+                        ELSIF input_usuario = matriz(2,2) then
                             pos4 := matriz(3,2);
-                        ELSIF (sw(6) & sw(5) & sw(4) & sw(3)) = matriz(0,3) then
+                        ELSIF input_usuario = matriz(0,3) then
                             pos4 := matriz(1,3);
-                        ELSIF (sw(6) & sw(5) & sw(4) & sw(3)) = matriz(1,3) then
+                        ELSIF input_usuario = matriz(1,3) then
                             pos4 := matriz(2,3);
-                        ELSIF (sw(6) & sw(5) & sw(4) & sw(3)) = matriz(2,3) then
+                        ELSIF input_usuario = matriz(2,3) then
                             pos4 := matriz(3,3);
                         END IF;
                     END IF;
@@ -284,34 +291,34 @@ BEGIN
 
         END IF; --MODO
 
-        -- Atribui saídas ao display de 7 segmentos
+        -- Atribui saídas ao hex0 de 7 segmentos
         if jogadas = 6 then
-            display1 <= "10111110";
+            hex1 <= "1011111";
         elsif jogadas = 5 then
-            display1 <= "10110110";
+            hex1 <= "1011011";
         elsif jogadas = 4 then
-            display1 <= "01100110";
+            hex1 <= "0110011";
         elsif jogadas = 3 then
-            display1 <= "11110010";
+            hex1 <= "1111001";
         elsif jogadas = 2 then
-            display1 <= "01100000";
+            hex1 <= "0110000";
         elsif jogadas = 1 then
-            display1 <= "11011010";
+            hex1 <= "1101101";
         elsif jogadas = 0 then
-            display1 <= "11111100";  
+            hex1 <= "1111110";  
             ledr <= "1111111111";   -- Efeito de derrota                      
         end if;
 
         if acertos_necessarios = 4 then
-            display <= "01100110";
+            hex0 <= "0110011";
         elsif jogadas = 3 then
-            display <= "11110010";
+            hex0 <= "1111001";
         elsif jogadas = 2 then
-            display <= "01100000";
+            hex0 <= "0110000";
         elsif jogadas = 1 then
-            display <= "11011010";
+            hex0 <= "1101101";
         elsif jogadas = 0 then
-            display <= "11111100";    
+            hex0 <= "1111110";    
             ledg <= "11111111";     -- Efeito de vitória
         end if;
 
